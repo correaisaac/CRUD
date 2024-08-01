d1b69f5124a1152129b6370f7f7875e977fd7bfb
correaisaac <correaisaac25@gmail.com>
2024-07-31 21:23:40 -0300

    feat: new fuction in the script.js

--- script.js
+++ script.js
@@ -1,15 +1,33 @@
 function logar() {
-
-
     var login = document.getElementById('login').value;
     var senha = document.getElementById('senha').value;
 
     if (login == 'admin' && senha == 'admin') {
-        alert('Pai ta on!!!')
+        alert('Bem vindo');
+        window.location.href = "./main-menu.html";
     } else {
         alert('Usuário ou senha incorretos');
     }
-
-
-
 }
+
+
+// Depois eu tento essa bomba
+// function verificaLogin(user, senha) {
+
+//     const userCorreto = "admin";
+//     const senhaCorreta = "admin";
+
+//     if (user === userCorreto && senha === senhaCorreta) {
+//         alert('Bem Vindo!')
+//         window.location.href = "./screens/main-menu.html";
+//     } else {
+
+//         alert("user ou senha incorretos!");
+//     }
+// }
+
+// document.getElementById('loginButton').addEventListener('click', function() {
+//     const user = document.getElementById('login').value;
+//     const senha = document.getElementById('senha').value;
+//     verificaLogin(user,  senha);
+// });
